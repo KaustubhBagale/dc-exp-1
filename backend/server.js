e70ec064aed13e67aaa16cc4edfa7046d2d1ebba
KaustubhBagale <163378232+KaustubhBagale@users.noreply.github.com>
2025-04-28 06:39:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,6 @@ app.delete("/tasks/:id", (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Backend running on port ${PORT}`);
 });
